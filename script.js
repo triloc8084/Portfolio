924e2178ea53b5aa0456e11033059ea79be95298
triloc8084 <trilochank856@gmail.com>
2026-03-24 13:12:11 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
 const overlay = document.getElementById("overlay");
 
-hamburger.addEventListener("click",()=>{
- hamburger.classList.toggle("active");
- navLinks.classList.toggle("active");
- overlay.classList.toggle("active");
-});
+if (hamburger && navLinks && overlay) {
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
 
-overlay.addEventListener("click",()=>{
- hamburger.classList.remove("active");
- navLinks.classList.remove("active");
- overlay.classList.remove("active");
-});
+  overlay.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+}
+
 
 
